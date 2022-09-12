84a9b1e99d640aad8a56ccec60abba62c36d5fc2
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 21:50:10 +0100

    constructing url to fetch data from api

--- Assets/css/js/index.js
+++ Assets/css/js/index.js
@@ -24,3 +24,24 @@ const writeToLocalStorage = (key, value) => {
   // set stringified value to LS for key name
   localStorage.setItem(key, stringifiedValue);
 };
+// constructing url to fetch data from api
+const constructUrl = (baseUrl, params) => {
+  const queryParams = new URLSearchParams(params).toString();
+
+  return queryParams ? `${baseUrl}?${queryParams}` : baseUrl;
+};
+
+const fetchData = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
