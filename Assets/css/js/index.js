f2366c86d076fe9e761960add637dc549c6cca75
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 21:53:14 +0100

    rendering forecast data

--- Assets/css/js/index.js
+++ Assets/css/js/index.js
@@ -117,3 +117,52 @@ const renderCurrentData = (data) => {
   
     weatherInfoContainer.append(currentWeatherCard);
   };
+  // forecast cards
+const renderForecastData = (data) => {
+    const createForecastCard = (each) => {
+      const forecast = `<div class="card m-2 forecast-card">
+          <div class="d-flex justify-content-center">
+            <img
+              src="http://openweathermap.org/img/w/${each.weather[0].icon}.png"
+              class="shadow-sm p-3 mt-3 bg-body rounded border card-img-top weather-icon"
+              alt="weather icon"
+            />
+            <div class="card-body">
+            <h5 class="card-title text-center">${moment
+              .unix(each.dt)
+              .format("ddd, Do MMM")}</h5>
+            <div class="mt-4 text-center">
+              <div class="row g-0">
+                <div class="col-12 p-2 border bg-light fw-bold">
+                  Temperature
+                </div>
+                <div class="col-12 p-2 border">${each.temp.day}&deg; C</div>
+              </div>
+              <div class="row g-0">
+              <div class="col-12 p-2 border bg-light fw-bold">
+                Humidity
+                </div>
+                <div class="col-12 p-2 border">${each.humidity}&percnt;</div>
+              </div>
+              <div class="row g-0">
+              <div class="col-12 p-2 border bg-light fw-bold">
+                Wind Speed
+                </div>
+                <div class="col-12 p-2 border">${each.wind_speed} MPH</div>
+              </div>
+              <div class="row g-0">
+              <div class="col-12 p-2 border bg-light fw-bold">
+                UV Index
+                </div>
+                <div class="col-12 p-2 border">
+                <span class="text-white px-3 rounded-2 ${getUviClassName(
+                  each.uvi
+                )}"
+                  >${each.uvi}</span
+                >
+                
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>`;
