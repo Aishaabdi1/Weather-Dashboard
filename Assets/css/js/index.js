0ed597d8391bb9340c3691f1c87ccbeaf5c3f41d
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-12 21:49:15 +0100

    write to local storage

---
+++ Assets/css/js/index.js
@@ -0,0 +1,26 @@
+// target parent
+console.log("hi from JS");
+const recentSearchesContainer = $("#recent-searches-container");
+const weatherInfoContainer = $("#weather-info-container");
+const searchForm = $("#search-form");
+
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+const writeToLocalStorage = (key, value) => {
+  // convert value to string
+  const stringifiedValue = JSON.stringify(value);
+
+  // set stringified value to LS for key name
+  localStorage.setItem(key, stringifiedValue);
+};
